3f2a1767bac4548594a8c6a636cc9f6c29c8f641
hamed <hamedp976@gmail.com>
2023-03-20 15:35:27 +0330

    029 Adding the code to get the product brands and types

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -25,4 +25,14 @@ public class ProductsController: ControllerBase
     {
         return Ok(await _productRepository.GetProductByIdAsync(id));
     }
+    [HttpGet("brands")]
+    public async Task<ActionResult<IReadOnlyList<Product>>> GetProductBrands()
+    {
+        return Ok(await _productRepository.GetProductBrandsAsync());
+    }
+    [HttpGet("types")]
+    public async Task<ActionResult<IReadOnlyList<Product>>> GetProductTypes()
+    {
+        return Ok(await _productRepository.GetProductTypesAsync());
+    }
 }
