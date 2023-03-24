6c8b92d545332215ff41eadfa64d5b94ad46e542
hamed <hamedp976@gmail.com>
2023-03-24 09:05:16 +0430

    050 Creating a consistent error response from the API

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -1,4 +1,5 @@
-﻿using Infrastructure.Data;
+﻿using API.Errors;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
@@ -16,7 +17,7 @@ public class BuggyController : BaseApiController
     {
         var thing = _context.Products.Find(42);
 
-        if (thing == null) return NotFound();
+        if (thing == null) return NotFound(new ApiResponse(404));
 
         return Ok();
     }
@@ -34,7 +35,7 @@ public class BuggyController : BaseApiController
     [HttpGet("badrequest")]
     public ActionResult GetBadRequest()
     {
-        return BadRequest();
+        return BadRequest(new ApiResponse(400));
     }
 
     [HttpGet("badrequest/{id}")]
