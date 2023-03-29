141b665a5590396eab05ecee78e5b9fa11760bf2
hamed <hamedp976@gmail.com>
2023-03-29 09:22:21 +0430

    175 Adding additional account methods

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,7 +1,9 @@
-﻿using API.Dtos;
+﻿using System.Security.Claims;
+using API.Dtos;
 using API.Errors;
 using Core.Entities.Identity;
 using Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -22,6 +24,38 @@ public class AccountController: BaseApiController
         _userManager = userManager;
         _signInManager = signInManager;
         _tokenService = tokenService;
+    }
+
+    [Authorize]
+    [HttpGet]
+    public async Task<ActionResult<UserDto>> GetCurrentUser()
+    {
+        var email = User.FindFirstValue(ClaimTypes.Email);
+        var email2 = HttpContext.User?.Claims?.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value;
+
+        var user = await _userManager.FindByEmailAsync(email ?? string.Empty);
+        return new UserDto
+        {
+            Email = user?.Email,
+            Token = _tokenService.CreateToken(user),
+            DisplayName = user?.DisplayName
+        };
+    }
+    
+    [HttpGet("emailexist")]
+    public async Task<ActionResult<bool>> CheckEmailExistAsync([FromQuery] string email)
+    {
+        return await _userManager.FindByEmailAsync(email) != null;
+    }
+    
+    [Authorize]
+    [HttpGet("address")]
+    public async Task<ActionResult<Address>> GetUserAddress()
+    {
+        var email = User.FindFirstValue(ClaimTypes.Email);
+
+        var user = await _userManager.FindByEmailAsync(email);
+        return user.Address;
     }
 
     [HttpPost("login")]
